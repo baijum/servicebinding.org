c9c28f4880f239061757adf005533a8a94d099d9
Baiju Muthukadan <baiju.m.mail@gmail.com>
2021-07-16 12:32:31 +0530

    prepare for publication

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,13 +5,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://servicebinding.org',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'baijum', // Usually your GitHub org/user name.
+  projectName: 'servicebinding.org', // Usually your repo name.
+  trailingSlash: false,
   themeConfig: {
     navbar: {
       title: 'My Site',
